8eddeffccdb813029c58c0c99d2a997ff05c7446
Pranish Dahal <pranish.dahal980@gmail.com>
2025-12-11 16:48:17 +0545

    added auth middleware and trip validator

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import USER_ROUTER from "./user.js";
 import BAGGAGE_ROUTER from "./baggage.js";
 import AUTH_Router from "./auth.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const Handlers = Router();
 
 Handlers.use("/users", USER_ROUTER);
 Handlers.use("/auth", AUTH_Router);
 Handlers.use("/baggage", BAGGAGE_ROUTER);
+
 
 export default Handlers;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js";
 import Handlers from "./handlers/index.js";
 import errorMiddleware from "./middlewares/error.js";
 import Auth_Router from "./handlers/auth.js";
+import { authMiddleware } from "./middlewares/auth.js";
 
 const APP_SERVER = express();
 
@@ -16,6 +17,7 @@ APP_SERVER.get("/", (req, res) => {
 });
 
 APP_SERVER.use(express.json());
+APP_SERVER.use(authMiddleware);
 APP_SERVER.use("/", Handlers);
 APP_SERVER.use(errorMiddleware);
 APP_SERVER.use(Auth_Router);
